e6fb549bf896a9921508013fd1ec250f77a2abb4
edpe <ed@edperkins.co.uk>
2021-07-02 11:43:04 +0100

    copy: add project description

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,32 @@ export const Home = () => {
       <div className={styles.container}>
         <div className={styles.title}>
           <h1>Inside Looking Out</h1>
-          <Link href="/main" passHref>
-            <a>Click to play</a>
-          </Link>
+          <div className={styles.main}>
+            <p>
+              Inside Looking Out uses live data from the UK Governments COVID-19
+              API as a narrative structure. Each day of the Coronavirus pandemic
+              in the UK is represented by a single animation frame, creating an
+              animated film that tells a story about the impact of the virus on
+              the people of the UK.
+            </p>
+            <p>
+              Each frame shows a photograph of a circular window, obscured by
+              horizontal lines representing each new case registered on that
+              day. As the frames advance, a pixel is removed to represent each
+              death, eroding the photograph until a final static image is
+              revealed representing cases and cumulative statistics from the
+              previous day.
+            </p>
+            <a
+              className={styles.inlineLink}
+              href="https://github.com/edpe/inside_looking_out"
+            >
+              Find out more and view the project on Github
+            </a>
+            <Link href="/main" passHref>
+              <a>PLAY</a>
+            </Link>
+          </div>
         </div>
       </div>
     </>
