4b59bc0fe95af78344f355cb37f357ee8c7ee2ca
edpe <ed@edperkins.co.uk>
2021-07-27 19:51:25 +0100

    add favicon and meta data

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,44 @@
-import '../styles/globals.css'
+import Head from "next/head";
+
+import "../styles/globals.css";
+
+const metaData = {
+  name: "Inside Looking Out",
+  description:
+    "Live data from the UK Government COVID-19 API is used as the source to create a narrative structure for this audiovisual web artwork.",
+  image: "https://inside-looking-out.vercel.app/img/meta-image.png",
+  url: "https://inside-looking-out.vercel.app/",
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Inside Looking Out</title>
+
+        <meta name="description" content={metaData.description} />
+
+        {/* Google / Search Engine Tags */}
+        <meta itemProp="name" content={metaData.name}></meta>
+        <meta itemProp="description" content={metaData.description} />
+        <meta itemProp="image" content={metaData.image} />
+
+        {/* Facebook Meta Tags */}
+        <meta property="og:url" content={metaData.url} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={metaData.name} />
+        <meta property="og:description" content={metaData.description} />
+        <meta property="og:image" content={metaData.image} />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={metaData.name} />
+        <meta name="twitter:description" content={metaData.description} />
+        <meta name="twitter:image" content={metaData.image}></meta>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
